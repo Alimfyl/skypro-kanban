adb0a0cf8b45f28c7fceb311608c8f72b94aa443
Almir Ishmukhametov <alimfyl@gmail.com>
2026-04-20 22:15:35 +0500

    Разобрал Main

---
+++ src/components/Column/Column.jsx
@@ -0,0 +1,15 @@
+import Card from "../Card/Card";
+
+function Column(props) {
+    return (
+        <div className="main__column">
+							<div className="column__title">
+								<p>{props.title}</p>
+							</div>
+							<div className="cards">
+								<Card />
+							</div>
+						</div>
+    )
+}
+export default Column
