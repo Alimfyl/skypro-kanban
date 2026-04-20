eef469ab04ddd04f62b87cbac5251ded23f48d2c
Almir Ishmukhametov <alimfyl@gmail.com>
2026-04-20 21:23:56 +0500

    appHeader

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+import Header from './components/Header/Header';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -279,32 +279,7 @@ function App() {
 		
 		
 
-		<header className="header">
-			<div className="container">
-				<div className="header__block">
-					<div className="header__logo _show _light">
-						<a href="" target="_self"><img src="images/logo.png" alt="logo"/></a>
-					</div>
-					<div className="header__logo _dark">
-						<a href="" target="_self"><img src="images/logo_dark.png" alt="logo"/></a>
-					</div>
-					<nav className="header__nav">
-						<button className="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать новую задачу</a></button>
-						<a href="#user-set-target" className="header__user _hover02">Ivan Ivanov</a>
-						<div className="header__pop-user-set pop-user-set" id="user-set-target">
-							    <a href="">x</a> 
-							<p className="pop-user-set__name">Ivan Ivanov</p>
-							<p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-							<div className="pop-user-set__theme">
-								<p>Темная тема</p>
-								<input type="checkbox" className="checkbox" name="checkbox"/>
-							</div>
-							<button type="button" className="_hover03"><a href="#popExit">Выйти</a></button>
-						</div>
-					</nav>					
-				</div>
-			</div>			
-		</header>
+		
 		<main className="main">
 			<div className="container">
 				
