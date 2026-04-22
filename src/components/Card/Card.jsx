3b26465071e5f775f7dc2afc8a856e5a3eec7ee6
Almir Ishmukhametov <alimfyl@gmail.com>
2026-04-22 21:57:51 +0500

    + Универсальная карточка.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,10 @@
-function Card() {
+function Card({theme, category, title, date}) {
     return (
         <div className="cards__item">
 									<div className="cards__card card">
 										<div className="card__group">
-											<div className="card__theme _orange">
-												<p className="_orange">Web Design</p>
+											<div className={`card__theme ${theme}`}>
+												<p className={theme}>{category}</p>
 											</div>
 											<a href="#popBrowse" target="_self">
 												<div className="card__btn">
@@ -16,7 +16,7 @@ function Card() {
 										</div>
 										<div className="card__content">
 											<a href="" target="_blank">
-												<h3 className="card__title">Название задачи</h3>
+												<h3 className="card__title">{title}</h3>
 											</a>
 											<div className="card__date">
 												<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
