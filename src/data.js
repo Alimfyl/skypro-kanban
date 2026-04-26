85141446e1273d35825ad2570a526a5c3387c529
Almir Ishmukhametov <alimfyl@gmail.com>
2026-04-26 13:55:20 +0500

    Создан файл data

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const cardList = [
+    {
+        id:1,
+        theme:"Web design",
+        title:"Название задачи",
+        date: "30.10.23",
+        status: "Без статуса"
+    },
+    {
+        id:2,
+        theme:"Research",
+        title:"Новая задача",
+        date: "30.10.23",
+        status: "Без статуса"
+    },
+    {
+        id:3,
+        theme:"Web design",
+        title:"Название задачи",
+        date: "30.10.23",
+        status: "Без статуса"
+    }
+    
+]
