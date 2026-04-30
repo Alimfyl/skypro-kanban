1e3f283dc5ef001524386c5bfa0d52ee51de3521
Almir Ishmukhametov <alimfyl@gmail.com>
2026-04-30 20:04:30 +0500

    +Card, начало проверок на работоспособность

--- CommonStyles.styled.js
+++ CommonStyles.styled.js
@@ -6,26 +6,3 @@ export const Container = styled.div`
   margin: 0 auto;
   padding: 0 30px;
 `;
-
-export const topicStyles = {
-  orange: {
-    backgroundColor: "#FFE4C2",
-    color: "#FF6D00",
-  },
-  green: {
-    backgroundColor: "#B4FDD1",
-    color: "#06B16E",
-  },
-  purple: {
-    backgroundColor: "#E9D4FF",
-    color: "#9A48F1",
-  },
-  gray: {
-    backgroundColor: "#94A6BE",
-    color: "#FFFFFF",
-  },
-};
-export const TopicText = styled.div`
-  background-color: ${({ $color }) => topicStyles[$color]?.backgroundColor || topicStyles.gray.backgroundColor};
-  color: ${({ $color }) => topicStyles[$color]?.color || topicStyles.gray.color};
-`;
