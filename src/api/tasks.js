cc07d23036952dde625fbf7cccc0b95e7284d2b6
Almir Ishmukhametov <alimfyl@gmail.com>
2026-05-08 23:28:09 +0500

    переделан tasks

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -1,12 +1,50 @@
-const baseHost = "https://wedev-api.sky.pro/api/kanban";
-export async function getTasks( {token} ) {
-    const response = await fetch(baseHost, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
+//const baseHost = "https://wedev-api.sky.pro/api/kanban";
+//export async function getTasks( {token} ) {
+//    const response = await fetch(baseHost, {
+//        headers: {
+//            Authorization: `Bearer ${token}`,
+//        },
+//    });
+//
+//    if (!response.ok) {
+//        throw new Error("Ошибка при получении задач")
+//    }
+//}
+import api from './index';
 
-    if (!response.ok) {
-        throw new Error("Ошибка при получении задач")
+
+export const fetchTasks = async () => {
+    try {
+        const {data} = await api.get('/kanban');
+        return data.tasks;
+    } catch (err) {
+        throw new Error(err.response?.data?.error || 'Не удалось загрузить задачи');
     }
-}
+};
+
+export const createTask = async (taskData) => {
+    try {
+        const {data} = await api.post('/kanban', taskData);
+        return data.tasks;
+    } catch (err) {
+        throw new Error(err.response?.data?.error || 'Ошибка при создании задачи');
+    }
+};
+
+export const updateTask = async (id, taskData) => {
+    try {
+        const {data} = await api.put(`/kanban/${id}`, taskData);
+        return data.tasks;
+    }catch (err) {
+        throw new Error(err.response?.data?.error || 'Ошибка при обновлении');
+    }
+};
+
+export const deleteTask = async (id) => {
+    try  {
+        const {data} = await api.delete(`/kanban/${id}`);
+        return data.tasks;
+    } catch (err) {
+        throw new Error(err.response?.data?.error || 'Не удалось удалить');
+    }
+};
