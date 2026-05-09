b4b422eafec64ae35e9800b27ee5d9bcc96f441d
Almir Ishmukhametov <alimfyl@gmail.com>
2026-05-09 23:49:41 +0500

    Оздан auth

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,2 +1,27 @@
+import api from './index'
+export const login = async ({ login, password }) => {
+    try {
+        const respose = await api.post('/user/login', {
+            login,
+            password,
+        });
+        return respose.data.user;
+    } catch (err) {
+        const message = err.response?.data?.error || 'Ошибка при входе';
+        throw new Error(message)
+    }
+};
 
-const apiUrlUser = "https:sky.pro"
+export const register = async ({ login, name, password }) => {
+    try{
+    const response = await api.post('/user', {
+    login,
+    name,
+    password,
+});
+return response.data.user;
+} catch (err) {
+    const message = err.response?.data?.error || 'Ошибка при регистрации';
+    throw new Error(message);
+}
+};
